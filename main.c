8b17d2362d4ce668ac9a4a69d3f73163381bfdaf
auuauu19697 <122807421+auuauu19697@users.noreply.github.com>
2023-02-03 09:36:23 +0000

    finished InsertionSort

--- main.c
+++ main.c
@@ -7,12 +7,12 @@ int main() {
   int a[N]={3,2,6,7,3,1};
   int i,j,new_number;
 
-   display(a,N);
-  bubbleSort(a,N); 
-   //insertion(a,N);
+  display(a,N);
+  //bubbleSort(a,N); 
+  insertion(a,N);
 
   // selectionSort(a,N);
-   display(a,N);
+  display(a,N);
  return 0;
 }
 
